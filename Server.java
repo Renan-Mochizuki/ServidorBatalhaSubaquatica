1a361f4bb8ebdb342d3ad4680eb0a2d214d99f5c
Renan-Mochizuki <renanmochizuki@gmail.com>
2025-10-20 02:01:04 -0300

    Adicionando thread simples de cliente

--- Server.java
+++ Server.java
@@ -57,31 +57,35 @@ class Jogador {
   }
 }
 
-class Dispositivo{
+class Dispositivo {
   Posicao posicao;
   int alcance;
 
-  Dispositivo(int x, int y, int alcance){
+  Dispositivo(int x, int y, int alcance) {
     this.posicao = new Posicao(x, y);
     this.alcance = alcance;
   }
 
-  Posicao getPosicao(){
+  Posicao getPosicao() {
     return this.posicao;
   }
 
-  int getAlcance(){
+  int getAlcance() {
     return this.alcance;
   }
 
-  Boolean detectar(Posicao posicao){
+  Boolean detectar(Posicao posicao) {
     return this.posicao.detectar(posicao, this.alcance);
   }
 }
 
 public class Server {
 
-  static void Imprimir(Jogador jogador1, Dispositivo dispositivo1) {
+  static Jogador jogador1;
+  static Dispositivo dispositivo1;
+
+  // Declarando metódo como synchronized para evitar condições de corrida
+  static synchronized void Imprimir(Jogador jogador1, Dispositivo dispositivo1) {
     System.out.println("  0 1 2 3 4 5 6 7 8 9");
     for (int i = 0; i < 10; i++) {
       System.out.print(i + " ");
@@ -98,45 +102,70 @@ public class Server {
     }
   }
 
-  public static void main(String[] args) throws Exception {
-    String sentence = "";
-    ServerSocket welcomeSocket = new ServerSocket(9876);
+  static class ClientThread extends Thread {
+    private Socket connectionSocket;
 
-    Jogador jogador1 = new Jogador("Alice", 0, 0);
-    Dispositivo dispositivo1 = new Dispositivo(-10, -10, 2);
-    Imprimir(jogador1, dispositivo1);
-    while (true) {
-      Socket connectionSocket = welcomeSocket.accept();
-      BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-      DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-      sentence = inFromClient.readLine();
-      System.out.println("RECEIVED: " + sentence);
-      int[] ataque = new int[]{ -1, -1 };
-      if (sentence.contains(" ")) {
-        String[] partes = sentence.split(" ");
-        ataque[0] = Integer.parseInt(partes[0]);
-        ataque[1] = Integer.parseInt(partes[1]);
-        dispositivo1 = new Dispositivo(ataque[1], ataque[0], 2);
-      }
-      switch (sentence) {
-        case "w":
-          jogador1.mover(0, -1);
-          break;
-        case "a":
-          jogador1.mover(-1, 0);
-          break;
-        case "s":
-          jogador1.mover(0, 1);
-          break;
-        case "d":
-          jogador1.mover(1, 0);
-          break;
-      }
-      Imprimir(jogador1, dispositivo1);
-      if(dispositivo1.detectar(jogador1.getPosicao())) {
-        System.out.println("Jogador detectado pelo dispositivo!");
+    public ClientThread(Socket socket) {
+      this.connectionSocket = socket;
+    }
+
+    public void run() {
+      try {
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+        String sentence;
+
+        while ((sentence = inFromClient.readLine()) != null) {
+          // Declara a classe Server como uma região crítica dessa parte do código
+          synchronized (Server.class) {
+            System.out.println("RECEIVED: " + sentence);
+            int[] ataque = new int[] { -1, -1 };
+            if (sentence.contains(" ")) {
+              String[] partes = sentence.split(" ");
+              ataque[0] = Integer.parseInt(partes[0]);
+              ataque[1] = Integer.parseInt(partes[1]);
+              dispositivo1 = new Dispositivo(ataque[1], ataque[0], 2);
+            }
+            switch (sentence) {
+              case "w":
+                jogador1.mover(0, -1);
+                break;
+              case "a":
+                jogador1.mover(-1, 0);
+                break;
+              case "s":
+                jogador1.mover(0, 1);
+                break;
+              case "d":
+                jogador1.mover(1, 0);
+                break;
+            }
+            Imprimir(jogador1, dispositivo1);
+            if (dispositivo1.detectar(jogador1.getPosicao())) {
+              System.out.println("Jogador detectado pelo dispositivo!");
+            }
+          }
+        }
+        connectionSocket.close();
+      } catch (Exception e) {
+        e.printStackTrace();
       }
     }
-    // connectionSocket.close();
+  }
+
+  public static void main(String[] args) throws Exception {
+    ServerSocket welcomeSocket = new ServerSocket(9876);
+
+    jogador1 = new Jogador("Alice", 0, 0);
+    dispositivo1 = new Dispositivo(-10, -10, 2);
+    Imprimir(jogador1, dispositivo1);
+
+    System.out.println("Servidor iniciado na porta 9876...");
+
+    while (true) {
+      Socket connectionSocket = welcomeSocket.accept();
+      ClientThread ClientThread = new ClientThread(connectionSocket);
+      ClientThread.start();
+    }
   }
 }
